678b0ea00cc45c880b6e8f96ed86a873c4817ff3
SHANKAR <shankarmalik21062003@gmail.com>
2022-02-11 22:04:16 +0530

    occurence

---
+++ Entropy/ques1/ques1.cpp
@@ -0,0 +1,40 @@
+// occurence
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s ;//= "1001100110011111";
+    getline(cin,s,';');
+    cin.ignore();
+	string ch ;//= "111"; 
+    getline(cin,ch,';');
+    cin.ignore();
+	int freq = 0;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		int j;
+		for (j = 0; j < ch.size(); j++)
+		{
+			if (s[i+j] != ch[j])
+			{
+				break;
+			}
+		}
+
+			if (j == ch.size())
+			{
+				freq++;
+			}
+		
+	}
+
+	cout << freq;
+
+
+	return 0;
+}
