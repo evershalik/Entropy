23b7aace13d88167fa84785dba2e44b65ce18898
SHANKAR <shankarmalik21062003@gmail.com>
2022-02-17 11:43:30 +0530

    new`1

--- Entropy/ques1/entropy.cpp
+++ Entropy/ques1/entropy.cpp
@@ -1,42 +1,68 @@
+#include <bits/stdc++.h>
 
-#include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
-    string s= "1010";
+    string s;
+    cout<<"Enter your binary string : "; cin>>s;
     int n=s.size();
     int one=0,zero=0;
+   
+    /*
     for (int i = 0; i < n; i++)
     {
         if(s[i]=='1')
         one++;
         else if(s[i]=='0') zero++;
     }
+    */
 
-    cout<<"no of ones are : "<<one<<endl;
-    cout<<"no of zeros are : "<<zero<<endl;
+    // cout<<"no of ones are : "<<one<<endl;
+    // cout<<"no of zeros are : "<<zero<<endl;
 
     int win_size=2;
-    int count;
+    cout<<"Enter size of window : "; cin>>win_size;
+    int count=0;
     
     for (int i = 0; i < n; i++)
     {
-        if((win_size + i) < n)// it give no of windows
+        if((win_size + i) <= n) // it give no of windows
         count++;
     }
 
     cout<<"no of windows formed are  : "<<count<<endl;
 
-    cout<<s<<endl;
+    // cout<<s<<endl;
+    // int coun1=0;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < count; i++)
     {
+        int k=0;
+        double temp_count=0;
+        string s1=s.substr(i);
+        // cout<<" new string s1 is : "<<s1<<endl;
+
         for (int  j = 0; j < win_size; j++)
         {
             
+            s1=s1.substr(k,win_size);
+            k=1;
+            // cout<<s1<<endl;
+            if(s1[0]=='1')
+            temp_count++;
+            // cout<<"size of temp_count is : "<<temp_count<<endl;
+            
+      
+            if(j==(win_size-1))
+            {
+                 // cout<<"s1 is empty "<<endl;
+                double entropy = ( -((temp_count / n) * log2f(temp_count / n)));
+                cout<<"entropy "<<i<<": "<< entropy <<endl;
+            }
+
+            
+
         }
         
     }
